fe0adace929eb481e8b08634cec7d70a48d2596f
Derekku01 <101446038+Derekku01@users.noreply.github.com>
2024-03-31 16:33:39 +0800

    Add files via upload

---
+++ pt1.js
@@ -0,0 +1,8 @@
+function Open() {
+    document.getElementById('eyesJS').src = 'Open.png';
+    document.getElementById('Description').style.display = 'block';
+}
+function Close() {
+    document.getElementById('eyesJS').src = 'Close.png';
+    document.getElementById('Description').style.display = 'None';
+}
